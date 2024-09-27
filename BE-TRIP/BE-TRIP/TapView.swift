80098a836596ec9b4469adc7eaca186aee09291c
동재 <59719500+jea0716@users.noreply.github.com>
2024-09-27 18:42:32 +0900

    feat : create bottomsheet view
    - create bottomsheet view

--- BE-TRIP/BE-TRIP/TapView.swift
+++ BE-TRIP/BE-TRIP/TapView.swift
@@ -6,8 +6,12 @@
 //
 
 import SwiftUI
+import BottomSheet
 
 struct TapView: View {
+    
+    @State var bottomSheetPosition: BottomSheetPosition = .relative(0.4)
+    
     var body: some View {
         TabView {
             ContentView()
@@ -16,6 +20,12 @@ struct TapView: View {
                     Text("홈")
                 }
             MapView()
+                .bottomSheet(
+                    bottomSheetPosition: self.$bottomSheetPosition,
+                    switchablePositions: [.relative(0.2), .relative(0.4), .relativeTop(0.95)],
+                    content: {
+                    
+                })
                 .tabItem {
                     Image(systemName: "2.square.fill")
                     Text("지도")
