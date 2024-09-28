b7486b962ed7097bcbcc1125519601604fa4d431
동재 <59719500+jea0716@users.noreply.github.com>
2024-09-28 21:40:58 +0900

    feat : 프로젝트 생성
    - 프로젝트 생성

---
+++ BF-TRIP/BF-TRIP/BF_TRIPApp.swift
@@ -0,0 +1,17 @@
+//
+//  BF_TRIPApp.swift
+//  BF-TRIP
+//
+//  Created by 박동재 on 9/28/24.
+//
+
+import SwiftUI
+
+@main
+struct BF_TRIPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BF-TRIP/BF-TRIP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BF-TRIP
+//
+//  Created by 박동재 on 9/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
