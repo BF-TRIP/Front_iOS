7a88c016da01cffbe5f0f48be397a1d0884b1211
동재 <59719500+jea0716@users.noreply.github.com>
2024-10-04 00:16:32 +0900

    feat : SearchView 구현, 연결
    - SearchView 구현, 연결

--- BF_TRIP/BF_TRIP/SearchBar.swift
+++ BF_TRIP/BF_TRIP/SearchBar.swift
@@ -9,21 +9,11 @@ import SwiftUI
 
 struct SearchBar: View {
     
-    @Binding var text: String
-    @State var editText: Bool = false
-    @State var isFilterViewShowing: Bool = false
+    @State var isSearchViewShowing: Bool = false
+    @State var text: String = ""
     
     var body: some View {
         HStack {
-            Button {
-                self.isFilterViewShowing = true
-            } label: {
-                Image(systemName: "line.3.horizontal")
-                    .foregroundColor(Color(.label))
-            }
-            
-            Spacer()
-
             TextField("검색어를 입력해주세요.", text: self.$text)
                 .padding(15)
                 .padding(.horizontal, 15)
@@ -32,41 +22,24 @@ struct SearchBar: View {
                 .overlay(
                     HStack {
                         Spacer()
-                        
-                        if self.editText {
-                            Button {
-                                //TODO: 검색기능 구현
-                            } label: {
-                                Image(systemName: "magnifyingglass")
-                                    .foregroundColor(Color(.label))
-                            }
-                            Button {
-                                self.editText = false
-                                self.text = ""
-                                UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-                            } label: {
-                                Image(systemName: "multiply.circle.fill")
-                                    .foregroundColor(Color(.label))
-                                    .padding()
-                            }
-                        } else {
-                            Button {
-                                //TODO: 음성녹음 띄우기
-                            } label: {
-                                Image(systemName: "mic")
-                                    .foregroundColor(Color(.label))
-                                    .padding()
-                            }
+                        Button {
+                            //TODO: 음성녹음 띄우기
+                        } label: {
+                            Image(systemName: "mic")
+                                .foregroundColor(Color(.label))
+                                .padding()
                         }
                     }
                 )
                 .onTapGesture {
-                    self.editText = true
+                    self.isSearchViewShowing = true
+                }
+                .fullScreenCover(isPresented: $isSearchViewShowing, content: {
+                    SearchView(isSearchViewShowing: $isSearchViewShowing, text: self.$text)
+                })
+                .transaction { transaction in
+                    transaction.disablesAnimations = true
                 }
         }
-        .padding(EdgeInsets(top: 5, leading: 15, bottom: 5, trailing: 15))
-        .fullScreenCover(isPresented: $isFilterViewShowing, content: {
-            SearchFilterView(isFilterViewShowing: $isFilterViewShowing)
-        })
     }
 }
---
+++ BF_TRIP/BF_TRIP/SearchView.swift
@@ -0,0 +1,85 @@
+//
+//  SearchView.swift
+//  BF_TRIP
+//
+//  Created by 박동재 on 10/3/24.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    
+    @Binding var isSearchViewShowing: Bool
+    @State private var searchText: String = ""
+    @Binding var text: String
+    @State var editText: Bool = false
+    
+    var body: some View {
+        NavigationStack {
+            HStack {
+                Button {
+                    //TODO: 뒤로가기
+                    isSearchViewShowing = false;
+                } label: {
+                    Image(systemName: "chevron.backward")
+                        .foregroundColor(Color(.label))
+                }
+                .padding()
+                
+                Spacer()
+                
+//                SearchBar(text: self.$searchText)
+                TextField("검색어를 입력해주세요.", text: self.$text)
+                    .padding(15)
+                    .padding(.horizontal, 15)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(15)
+                    .overlay(
+                        HStack {
+                            Spacer()
+                            
+                            if self.editText {
+                                Button {
+                                    //TODO: 검색기능 구현
+                                } label: {
+                                    Image(systemName: "magnifyingglass")
+                                        .foregroundColor(Color(.label))
+                                }
+                                Button {
+                                    self.editText = false
+                                    self.text = ""
+                                    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                                } label: {
+                                    Image(systemName: "multiply.circle.fill")
+                                        .foregroundColor(Color(.label))
+                                        .padding()
+                                }
+                            } else {
+                                Button {
+                                    //TODO: 음성녹음 띄우기
+                                } label: {
+                                    Image(systemName: "mic")
+                                        .foregroundColor(Color(.label))
+                                        .padding()
+                                }
+                            }
+                        }
+                    )
+                    .onTapGesture {
+                        self.editText = true
+                    }
+            }
+            .padding(EdgeInsets(top: 5, leading: 15, bottom: 5, trailing: 15))
+            
+            Spacer()
+            
+            Text("관광지를 검색해보세요!")
+            
+            Spacer()
+        }
+        .transaction { transaction in
+            transaction.disablesAnimations = true
+        }
+    }
+    
+}
