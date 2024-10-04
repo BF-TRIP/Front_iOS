81eb92b751fc72d0218991e7681c1f1e87fa609e
동재 <59719500+jea0716@users.noreply.github.com>
2024-10-04 21:13:36 +0900

    feat : 필터 컴포넌트화 및 버튼 이펙트 구현
    - 필터 컴포넌트화 및 버튼 이펙트 구현

--- BF_TRIP/BF_TRIP/PlaceListView.swift
+++ BF_TRIP/BF_TRIP/PlaceListView.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 struct PlaceListView: View {
     
+    private let stateList: [String] = ["전체", "지체장애", "고령자", "영유아동반자", "시각장애", "청각장애"]
+    @State private var selectedStateList: [Bool] = [false, false, false, false, false, false,]
+    @State private var selectedStates = [Int]()
+    
     var body: some View {
         VStack {
             HStack {
@@ -26,91 +30,46 @@ struct PlaceListView: View {
                         .foregroundColor(Color(.label))
                         .frame(width: 24, height: 24)
                 }
-
+                
             }
             .padding()
             
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack {
-                    //TODO: 누를때 색 변경, 컴포넌트화 추후 개발
-                    Button {
-                    
-                    } label: {
-                        Text("전체")
-                            .font(.system(size: 14))
-                            .foregroundColor(Color(.label))
-                            .padding(EdgeInsets(top: 5, leading: 15, bottom: 5, trailing: 15))
+                    ForEach(0..<stateList.count, id: \.self) { index in
+                        Button {
+                            if self.selectedStates.contains(index) {
+                                self.selectedStates.removeAll { $0 == index }
+                            } else {
+                                self.selectedStates.append(index)
+                            }
+                        } label: {
+                            Text("\(self.stateList[index])")
+                                .font(.system(size: 14))
+                                .foregroundStyle(self.selectedStates.contains(index) == true ? Color(hex: "#FFE023") : Color(.label))
+                                .padding(EdgeInsets(top: 5, leading: 15, bottom: 5, trailing: 15))
+                        }
+                        .background(self.selectedStates.contains(index) ? Color(hex: "#393939") : Color(.clear))
+                        .cornerRadius(100)
                     }
                     .overlay(RoundedRectangle(cornerRadius: 100)
                         .stroke(lineWidth: 1)
                     )
                     .padding(EdgeInsets(top: 5, leading: 2, bottom: 5, trailing: 2))
-                    
-                    Button {
-                    
-                    } label: {
-                        Text("지체장애")
-                            .font(.system(size: 14))
-                            .foregroundColor(Color(.label))
-                            .padding(EdgeInsets(top: 5, leading: 15, bottom: 5, trailing: 15))
-                    }
-                    .overlay(RoundedRectangle(cornerRadius: 100)
-                        .stroke(lineWidth: 1)
-                    )
-                    .padding(EdgeInsets(top: 5, leading: 2, bottom: 5, trailing: 2))
-                    
-                    Button {
-                    
-                    } label: {
-                        Text("고령자")
-                            .font(.system(size: 14))
-                            .foregroundColor(Color(.label))
-                            .padding(EdgeInsets(top: 5, leading: 15, bottom: 5, trailing: 15))
-                    }
-                    .overlay(RoundedRectangle(cornerRadius: 100)
-                        .stroke(lineWidth: 1)
-                    )
-                    .padding(EdgeInsets(top: 5, leading: 2, bottom: 5, trailing: 2))
-                    
-                    Button {
-                    
-                    } label: {
-                        Text("시각장애")
-                            .font(.system(size: 14))
-                            .foregroundColor(Color(.label))
-                            .padding(EdgeInsets(top: 5, leading: 15, bottom: 5, trailing: 15))
-                    }
-                    .overlay(RoundedRectangle(cornerRadius: 100)
-                        .stroke(lineWidth: 1)
-                    )
-                    .padding(EdgeInsets(top: 5, leading: 2, bottom: 5, trailing: 2))
-                    
-                    Button {
-                    
-                    } label: {
-                        Text("영유아 동반자")
-                            .font(.system(size: 14))
-                            .foregroundColor(Color(.label))
-                            .padding(EdgeInsets(top: 5, leading: 15, bottom: 5, trailing: 15))
-                    }
-                    .overlay(RoundedRectangle(cornerRadius: 100)
-                        .stroke(lineWidth: 1)
-                    )
-                    .padding(EdgeInsets(top: 5, leading: 2, bottom: 5, trailing: 2))
-                    
                 }
             }
             .padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 15))
             
-
             List(0..<5) {_ in
                 PlaceView()
                     .padding(EdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 0))
+                    .listRowSeparator(.hidden)
             }
             .scrollIndicators(.hidden)
             .listStyle(PlainListStyle())
         }
         .padding(EdgeInsets(top: 0, leading: 0, bottom: 100, trailing: 0))
+        
     }
     
 }
