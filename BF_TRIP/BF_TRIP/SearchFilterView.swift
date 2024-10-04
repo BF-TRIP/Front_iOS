b7c05fb2d1a3a802be26e110965ef59041c77758
동재 <59719500+jea0716@users.noreply.github.com>
2024-10-04 16:59:20 +0900

    feat : 지역 리스트, 터치 시 색 변화 구현
    - 지역 리스트, 터치 시 색 변화 구현

--- BF_TRIP/BF_TRIP/SearchFilterView.swift
+++ BF_TRIP/BF_TRIP/SearchFilterView.swift
@@ -9,6 +9,32 @@ import SwiftUI
 
 struct SearchFilterView: View {
     @Binding var isFilterViewShowing: Bool
+    @State private var selectedState: String? = nil
+    @State private var selectedStateIndex: Int? = nil
+    @State private var selectedCity: String? = nil
+    
+    private let stateList: [String] = ["서울", "경기", "인천", "강원", "충북", "충남", "경북", "경남", "전북", "전남", "제주"]
+    
+    //TODO: 광역시에 대한 것도 할건 지
+    private let cityList: [[String]] = [
+        ["전체", "종로구", "중구", "용산구", "성동구", "광진구", "동대문구", "중랑구", "성북구", "강북구", "도봉구", "노원구", "은평구", 
+         "서대문구", "마포구", "양천구", "강서구", "구로구", "금천구", "영등포구", "동작구", "관악구", "서초구", "강남구", "송파구", "강동구"],
+        ["전체", "고양시", "파주시", "김포시", "의정부시", "양주시", "포천시", "동두천시", "남양주시", "구리시", "광주시", "하남시", "이천시", "여주시", "수원시", 
+         "성남시", "용인시", "안성시", "과천시", "안양시", "의왕시", "군포시", "화성시", "평택시", "부천시", "광명시", "시흥시", "안산시", "오산시"],
+        ["전체", "중구", "동구", "미추홀구", "연수구", "남동구", "부평구", "계양구", "서구", "강화군", "웅진군"],
+        ["전체", "원주시", "춘천시", "강릉시", "동해시", "속초시", "홍천군", "삼척시", "횡성군", "철원군", 
+         "평창군", "태백시", "영월군", "정선군", "인제군", "양양군", "고성군", "화천군", "양구군"],
+        ["전체", "청주시", "충주시", "제천시", "보은군", "증평군", "옥천군", "영동군", "괴산군", "진천군", "음성군", "단양군"],
+        ["전체", "천안시", "아산시", "서산시", "당진시", "공주시", "보령시", "논산시", "계룡시",
+         "흥성군", "예산군", "부여군", "서천군", "청양군", "태안군", "금산군"],
+        ["전체", "포항시", "경주시", "안동시", "김천시", "구미시", "영주시", "영천시", "상주시", "문경시", "경산시", "의정군",
+         "청송군", "영양군", "영덕군", "청도군", "고령군", "성주군", "칠곡군", "예천군", "봉화군", "울진군", "울릉군"],
+        ["전체", "창원시", "진주시", "김해시", "양산시", "거제시", "통영시", "사천시", "밀양시", "의령군",
+         "함안군", "창녕군", "고성군", "남해군", "하동군", "산청군", "함양군", "거창군", "합천군"],
+        ["전체", "전주시", "군산시", "익산시", "정읍시", "남원시", "김제시", "완주군", "진안군", "무주군", "장수군", "임실군", "순창군", "고창군", "부안군"],
+        ["전체", "목포시", "여수시", "순천시", "나주시", "광양시", "담양군", "곡성군", "구례군", "고흥군", "보성군", "화순군", "장흥군", "강진군", "해남군", "영암군", "무안군", "함평군", "영광군", "장성군", "완도군", "진도군", "신안군"],
+        ["전체", "제주시", "서귀포시"]
+    ]
     
     var body: some View {
         VStack {
@@ -27,43 +53,60 @@ struct SearchFilterView: View {
             Spacer()
             
             Text("원하는 여행지의")
-                .fontWeight(.bold)
-                .font(.system(size: 24))
+                .font(.system(size: 20))
                 .frame(maxWidth: .infinity, alignment: .leading)
                 .padding(EdgeInsets(top: 0, leading: 16, bottom: 0, trailing: 0))
             Text("위치를 검색해주세요")
-                .fontWeight(.bold)
-                .font(.system(size: 24))
+                .font(.system(size: 20))
                 .frame(maxWidth: .infinity, alignment: .leading)
                 .padding(EdgeInsets(top: 0, leading: 16, bottom: 0, trailing: 0))
             
             Spacer()
             
             HStack {
-                //TODO: 지역, 상세지역 리스트 만들어서 적용
-                //TODO: 눌렀을 때, 애니메이션이랑 색 변경
-                List(0..<8) { i in
-                    Text("\(i)")
-                        .listRowBackground(Color(.systemGray5))
+                List(0..<self.stateList.count, id: \.self) { index in
+                    Text("\(stateList[index])")
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .font(.system(size: 16))
+                        .foregroundColor(self.selectedState == stateList[index] ? Color(.label) : Color(hex: "#949494"))
+                        .listRowBackground(self.selectedState == stateList[index] ? Color(hex: "#FFE023") : Color(hex: "#E2E2E2"))
+                        .listRowSeparator(.hidden)
+                        .onTapGesture {
+                            self.selectedState = stateList[index]
+                            self.selectedStateIndex = index
+                        }
                 }
                 .listStyle(PlainListStyle())
-                .frame(width: 120, height: 400)
+                .frame(width: 120, height: CGFloat(self.stateList.count) * 50)
                 .scrollDisabled(true)
-                List(0..<30) { i in
-                    Text("\(i)")
+                if let stateIndex = selectedStateIndex {
+                    List(0..<cityList[stateIndex].count, id: \.self) { index in
+                        Text("\(cityList[stateIndex][index])")
+                            .listRowBackground(self.selectedCity == cityList[stateIndex][index] ? Color(hex: "#FFFCE7") : Color(.white))
+                            .onTapGesture {
+                                self.selectedCity = cityList[stateIndex][index]
+                            }
+                    }
+                    .listStyle(PlainListStyle())
+                    .frame(width: 240, height: CGFloat(self.stateList.count) * 50)
+                    .scrollBounceBehavior(.basedOnSize)
+                } else {
+                    List {
+                        EmptyView()
+                    }
+                    .listStyle(PlainListStyle())
+                    .frame(width: 240, height: CGFloat(self.stateList.count) * 50)
+                    .scrollBounceBehavior(.basedOnSize)
                 }
-                .listStyle(PlainListStyle())
-                .frame(width: 240, height: 400)
-                .scrollBounceBehavior(.basedOnSize)
             }
-            .frame(height: 400)
+            .frame(height: CGFloat(self.stateList.count) * 50)
             
             Spacer()
             
             HStack {
                 Button {
-                    //TODO: 지역, 상세지역 저장하고 전달
-                    dump("ACCEPT")
+                    //TODO: API 통신 후 받은 데이터 결과 화면 이동
+                    dump("\(String(describing: self.selectedState)) \(String(describing: self.selectedCity))")
                     self.isFilterViewShowing = false
                 } label: {
                     Text("적용하기")
