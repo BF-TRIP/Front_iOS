bb1f55a20e3bdbab777d2c3758ae18ed7e3e3e56
동재 <59719500+jea0716@users.noreply.github.com>
2024-10-09 01:52:36 +0900

    feat : LocationMapAnnotation 생성
    - LocationMapAnnotation 생성

---
+++ BF_TRIP/BF_TRIP/Custom/LocationMapAnnotation.swift
@@ -0,0 +1,32 @@
+//
+//  LocationMapAnnotation.swift
+//  BF_TRIP
+//
+//  Created by 박동재 on 10/9/24.
+//
+
+import SwiftUI
+
+struct LocationMapAnnotation: View {
+    var body: some View {
+        VStack(spacing: 0) {
+            Image(systemName: "map.circle.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 30, height: 30)
+                .font(.headline)
+                .padding(6)
+                .foregroundColor(.white)
+                .background(Color(hex: "#FFE023"))
+                .cornerRadius(36)
+            
+            Image(systemName: "triangle.fill")
+                .resizable()
+                .scaledToFit()
+                .foregroundColor(Color(hex: "#FFE023"))
+                .frame(width: 10, height: 10)
+                .rotationEffect(Angle(degrees: 180))
+                .offset(y: -3)
+        }
+    }
+}
