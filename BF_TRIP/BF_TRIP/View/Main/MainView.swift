db6bf8719e90b53fe3c2acbf21ae4e37f2139df9
동재 <59719500+jea0716@users.noreply.github.com>
2024-10-10 03:25:36 +0900

    feat : 웹 통신

--- BF_TRIP/BF_TRIP/View/Main/MainView.swift
+++ BF_TRIP/BF_TRIP/View/Main/MainView.swift
@@ -9,18 +9,24 @@ import SwiftUI
 
 struct MainView: View {
     
-    @State private var isVoiceViewShowing = false
-        
+    @State var isVoiceViewShowing: Bool = false
+    
     var body: some View {
-        WebKit(
-            request: URLRequest(url: URL(string: "http://localhost:5173/")!),
-            isVoiceViewShowing: $isVoiceViewShowing
-        )
+        let webView = WebKit(
+                request: URLRequest(url: URL(string: "http://localhost:5173/home")!),
+                isVoiceViewShowing: $isVoiceViewShowing
+            )
+        
+        return webView
         .fullScreenCover(isPresented: $isVoiceViewShowing, content: {
             VoiceView(isVoiceViewShowing: $isVoiceViewShowing)
         })
         .transaction { transaction in
             transaction.disablesAnimations = true
         }
+        .onAppear(perform: {
+            webView.callJS(gpsX: 126.98, gpsY: 37.57)
+            dump("123123")
+        })
     }
 }
