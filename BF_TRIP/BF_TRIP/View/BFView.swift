d8131034339fbb4eaf819bb6577e79a6265d1de1
동재 <59719500+jea0716@users.noreply.github.com>
2024-11-16 01:04:09 +0900

    feat : loading 화면 추가

--- BF_TRIP/BF_TRIP/View/BFView.swift
+++ BF_TRIP/BF_TRIP/View/BFView.swift
@@ -14,6 +14,7 @@ struct BFView: View {
     @StateObject var mapViewModel: MapViewModel = MapViewModel()
     @State var emtpyShowing: Bool = false
     @State var isOnboarding: Bool = false
+    @State var loading: Bool = false
     
     init() {
         UITabBar.appearance().backgroundColor = UIColor(.white)
@@ -21,44 +22,54 @@ struct BFView: View {
     }
     
     var body: some View {
-        if !isOnboarding {
-            OnboardingView(isOnboarding: $isOnboarding)
-        } else {
-            TabView {
-                MainView(gpsX: mapViewModel.gpsX, gpsY: mapViewModel.gpsY)
-                    .tabItem {
-                        Image(systemName: "house")
-                        Text("홈")
-                    }
-                MapView(viewModel: mapViewModel)
-                    .bottomSheet(
-                        bottomSheetPosition: self.$bottomSheetPosition,
-                        switchablePositions: [.relative(0.3), .relative(0.5), .relativeTop(0.95)],
-                        content: {
-                            PlaceListView(
-                                title: "관광지 목록",
-                                searching: false,
-                                isPlaceListViewShowing: $emtpyShowing,
-                                viewModel: self.mapViewModel
-                            )
-                            .padding(.bottom, 100)
-                        })
-                    .tabItem {
-                        Image(systemName: "map")
-                        Text("지도")
-                    }
-                BookmarkView()
-                    .tabItem {
-                        Image(systemName: "bookmark")
-                        Text("저장")
-                    }
-                    .background(Color(hex: "#000000"))
-                    .background(ignoresSafeAreaEdges: .all)
+        if !loading {
+            VStack {
             }
-            .accentColor(Color(.label))
             .onAppear(perform: {
-                self.mapViewModel.requestRegion()
             })
+        } else {
+            if !isOnboarding {
+                OnboardingView(isOnboarding: $isOnboarding)
+            } else {
+                TabView {
+                    MainView(gpsX: mapViewModel.gpsX, gpsY: mapViewModel.gpsY)
+                        .tabItem {
+                            Image(systemName: "house")
+                            Text("홈")
+                        }
+                    MapView(viewModel: mapViewModel)
+                        .bottomSheet(
+                            bottomSheetPosition: self.$bottomSheetPosition,
+                            switchablePositions: [.relative(0.3), .relative(0.5), .relativeTop(0.95)],
+                            content: {
+                                PlaceListView(
+                                    title: "관광지 목록",
+                                    searching: false,
+                                    isPlaceListViewShowing: $emtpyShowing,
+                                    viewModel: self.mapViewModel
+                                )
+                                .padding(.bottom, 100)
+                            })
+                        .tabItem {
+                            Image(systemName: "map")
+                            Text("지도")
+                        }
+                    BookmarkView()
+                        .tabItem {
+                            Image(systemName: "bookmark")
+                            Text("저장")
+                        }
+                        .background(Color(hex: "#000000"))
+                        .background(ignoresSafeAreaEdges: .all)
+                }
+                .accentColor(Color(.label))
+                .onAppear(perform: {
+                    self.mapViewModel.requestRegion()
+                })
+            }
+        }
+    }
+    
         }
     }
 }
