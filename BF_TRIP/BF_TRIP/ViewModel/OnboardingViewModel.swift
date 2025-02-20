8519fda37dc70a6f261511fd3982889563318d7f
동재 <59719500+jea0716@users.noreply.github.com>
2025-02-20 19:33:41 +0900

    feat : OnboardingViewModel 구현

---
+++ BF_TRIP/BF_TRIP/ViewModel/OnboardingViewModel.swift
@@ -0,0 +1,60 @@
+//
+//  OnboardingViewModel.swift
+//  BF_TRIP
+//
+//  Created by 박동재 on 2/18/25.
+//
+
+import Foundation
+
+final class OnboardingViewModel: ObservableObject {
+    
+    @Published private(set) var name: String = ""
+    @Published private(set) var birth: Date = Date()
+    @Published private(set) var gender: Int? = nil
+    @Published private(set) var disabilityList: [Bool] = Array(repeating: false, count: 4)
+    @Published private(set) var tripList: [Bool] = Array(repeating: false, count: 4)
+    
+    private var selectedDisabilities: [Int] {
+        disabilityList.enumerated()
+            .filter { $0.element }
+            .map { $0.offset + 1 }
+    }
+    
+    private var selectedTripTypes: [Int] {
+        tripList.enumerated()
+            .filter { $0.element }
+            .map { $0.offset + 1 }
+    }
+    
+    private var selectedGender: String {
+        gender == 0 ? "man" : "woman"
+    }
+    
+    func updateName(_ newValue: String) {
+        if newValue.count > 5 {
+            name = String(newValue.prefix(5))
+        } else {
+            name = newValue
+        }
+    }
+    
+    func updateBirth(_ newBirth: Date) {
+        birth = newBirth
+    }
+    
+    func updateGender(_ newGender: Int?) {
+        gender = newGender
+    }
+    
+    func toggleDisability(at index: Int) {
+        guard index >= 0 && index < disabilityList.count else { return }
+        disabilityList[index].toggle()
+    }
+    
+    func toggleTrip(at index: Int) {
+        guard index >= 0 && index < tripList.count else { return }
+        tripList[index].toggle()
+    }
+    
+}
