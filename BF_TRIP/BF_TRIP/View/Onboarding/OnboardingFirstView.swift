039bdfafffc79b293d64d6531fb7a5122a2d8251
동재 <59719500+jea0716@users.noreply.github.com>
2025-02-20 19:37:10 +0900

    feat : 온보딩 첫 번째 화면 구성

---
+++ BF_TRIP/BF_TRIP/View/Onboarding/OnboardingFirstView.swift
@@ -0,0 +1,44 @@
+//
+//  OnboardingFirstView.swift
+//  BF_TRIP
+//
+//  Created by 박동재 on 2/4/25.
+//
+
+import SwiftUI
+import Combine
+
+struct OnboardingFirstView: View {
+    
+    @ObservedObject var viewModel: OnboardingViewModel
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("가입을 축하드려요🎉")
+                .font(.system(size: 24, weight: .semibold))
+            Text("어떻게 불러드리면 될까요?")
+                .font(.system(size: 24, weight: .semibold))
+        }
+        .padding()
+        .frame(maxWidth: .infinity, alignment: .leading)
+        
+        Spacer()
+        
+        TextField("5글자 내로 입력해주세요.", text: Binding(
+            get: { viewModel.name },
+            set: { viewModel.updateName($0) }
+        ))
+        .overlay(
+            HStack {
+                Spacer()
+                Text("\(viewModel.name.count)/5")
+                    .foregroundColor(.gray)
+                    .padding(.trailing, 8)
+            }
+        )
+        .font(.system(size: 20))
+        .padding(.horizontal)
+        .padding(10)
+        .submitLabel(.done)
+    }
+}
