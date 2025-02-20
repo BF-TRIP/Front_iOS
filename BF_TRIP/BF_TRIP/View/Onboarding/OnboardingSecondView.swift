7d9e0e23a4e1b285c44e6fc34a7c4d272f96ae52
동재 <59719500+jea0716@users.noreply.github.com>
2025-02-20 19:37:16 +0900

    feat : 온보딩 두 번째 화면 구성

---
+++ BF_TRIP/BF_TRIP/View/Onboarding/OnboardingSecondView.swift
@@ -0,0 +1,42 @@
+//
+//  OnboardingSecondView.swift
+//  BF_TRIP
+//
+//  Created by 박동재 on 2/9/25.
+//
+
+import SwiftUI
+
+struct OnboardingSecondView: View {
+    
+    @ObservedObject var viewModel: OnboardingViewModel
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("\(viewModel.name) 님의 생년월일을")
+                .font(.system(size: 24, weight: .semibold))
+            Text("알려주세요.")
+                .font(.system(size: 24, weight: .semibold))
+            Text("연령대에 맞는 맞춤 추천을 제공해드려요.")
+                .font(.system(size: 20))
+                .foregroundColor(Color(hex: "6E6E6E"))
+                .padding(.top, 5)
+        }
+        .padding()
+        .frame(maxWidth: .infinity, alignment: .leading)
+        
+        Spacer()
+        
+        DatePicker(
+            "",
+            selection: Binding(
+                get: { viewModel.birth },
+                set: { viewModel.updateBirth($0) }
+            ),
+            in: ...Date.now,
+            displayedComponents: .date
+        )
+        .datePickerStyle(WheelDatePickerStyle())
+        .labelsHidden()
+    }
+}
