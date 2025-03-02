f49f8c77a35397401efe8a812517835626a617be
동재 <59719500+jea0716@users.noreply.github.com>
2025-03-02 14:58:22 +0900

    feat : 코스 온보딩 화면 구성

---
+++ BF_TRIP/BF_TRIP/View/Course/CourseMainView.swift
@@ -0,0 +1,122 @@
+//
+//  CourseMainView.swift
+//  BF_TRIP
+//
+//  Created by 박동재 on 2/20/25.
+//
+
+import SwiftUI
+
+struct CourseMainView: View {
+    
+    @State private var isButtonEnabled = true
+    @State private var isCreateViewShowing = false
+    @State private var isOnboardingState = false
+    
+    var body: some View {
+        GeometryReader { geometry in
+            VStack(spacing: 0) {
+                Image(uiImage: .aiCourse)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: geometry.size.height * 0.4)
+                    .padding(.top, geometry.size.height * 0.05)
+                
+                VStack(spacing: 20) {
+                    CourseCustomButton(
+                        color: Color(hex: "#FF8C00"),
+                        descriptionColor: Color.black,
+                        title: "빠르게 생성하기",
+                        description1: "입력한 정보를 바탕으로",
+                        description2: "맞춤형 코스를 만들어드려요.",
+                        image: Image(uiImage: .clock)
+                    ) {
+                        isButtonEnabled = false
+                        isOnboardingState = true
+                        isCreateViewShowing = true
+                        
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                            isButtonEnabled = true
+                        }
+                    }
+                    .disabled(!isButtonEnabled)
+                    .fullScreenCover(isPresented: $isCreateViewShowing) {
+                        CourseOnboardingMainView(onboardingState: $isOnboardingState, isShowing: $isCreateViewShowing)
+                    }
+                    
+                    CourseCustomButton(
+                        color: Color(hex: "#323232"),
+                        descriptionColor: Color.white,
+                        title: "세부 조건 다시 고르기",
+                        description1: "원하는 조건을 다시 선택하여",
+                        description2: "나만의 코스를 만들어드려요.",
+                        image: Image(uiImage: .target)
+                    ) {
+                        isButtonEnabled = false
+                        isOnboardingState = false
+                        isCreateViewShowing = true
+                        
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                            isButtonEnabled = true
+                        }
+                    }
+                    .disabled(!isButtonEnabled)
+                    .fullScreenCover(isPresented: $isCreateViewShowing) {
+                        CourseOnboardingMainView(onboardingState: $isOnboardingState, isShowing: $isCreateViewShowing)
+                    }
+                }
+                .padding(.leading)
+                .padding(.trailing)
+                .padding(.bottom)
+            }
+        }
+        .background(Color(hex: "FFFCE7"))
+        .ignoresSafeArea()
+    }
+
+}
+
+struct CourseCustomButton: View {
+    let color: Color
+    let descriptionColor: Color
+    let title: String
+    let description1: String
+    let description2: String
+    let image: Image
+    let action: () -> Void
+    
+    var body: some View {
+        Button(action: action) {
+            ZStack(alignment: .bottomLeading) {
+                color
+            
+                VStack(alignment: .leading, spacing: 5) {
+                    Spacer()
+                    Text(title)
+                        .font(.system(size: 24, weight: .bold))
+                        .foregroundColor(.white)
+                    
+                    Spacer().frame(height: 20)
+                    
+                    Text(description1)
+                        .font(.system(size: 16, weight: .medium))
+                        .foregroundColor(descriptionColor)
+                    Text(description2)
+                        .font(.system(size: 16, weight: .medium))
+                        .foregroundColor(descriptionColor)
+                }
+                .padding(.bottom, 30)
+                .padding(.leading, 30)
+                
+                image
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 80, height: 80)
+                    .position(x: UIScreen.main.bounds.width - 90, y: 90)
+            }
+            .frame(height: 160)
+            .cornerRadius(10)
+        }
+        .frame(maxWidth: .infinity)
+    }
+}
---
+++ BF_TRIP/BF_TRIP/View/Course/CourseOnboardingSecondView.swift
@@ -0,0 +1,48 @@
+//
+//  CourseOnboardingSecondView.swift
+//  BF_TRIP
+//
+//  Created by 박동재 on 2/23/25.
+//
+
+import SwiftUI
+
+struct CourseOnboardingSecondView: View {
+    
+    @ObservedObject var viewModel: OnboardingViewModel
+    @State private var selectedButton: String? = nil
+    let buttonTitles = ["당일치기", "1박 2일", "2박 3일"]
+    
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("얼마나 떠날까요?")
+                .font(.system(size: 24, weight: .semibold))
+        }
+        .padding()
+        .frame(maxWidth: .infinity, alignment: .leading)
+        
+        LazyVGrid(columns: columns, spacing: 10) {
+            ForEach(buttonTitles, id: \.self) { button in
+                ButtonView(button: button, isSelected: selectedButton == button, action: {
+                    if selectedButton == button {
+                        selectedButton = nil
+                        viewModel.updateDays(nil)
+                    } else {
+                        selectedButton = button
+                        viewModel.updateDays(button)
+                    }
+                })
+            }
+        }
+        .padding()
+        
+        Spacer()
+        Spacer()
+    }
+}
