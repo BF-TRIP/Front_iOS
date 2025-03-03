47481ac75a196fedd095ae673dcc055ad8052769
동재 <59719500+jea0716@users.noreply.github.com>
2025-03-03 16:00:33 +0900

    feat : 코스 온보딩 마지막 화면 구성

---
+++ BF_TRIP/BF_TRIP/View/Course/Onboarding/CourseOnboardingFinalView.swift
@@ -0,0 +1,79 @@
+//
+//  CourseOnboardingFinalView.swift
+//  BF_TRIP
+//
+//  Created by 박동재 on 3/3/25.
+//
+
+import SwiftUI
+
+struct CourseOnboardingFinalView: View {
+    
+    @State private var currentTextIndex = 0
+    private let textMessages = [
+        "조금만 기다려주세요",
+        "더 나은 접근성을 위한",
+        "맞춤 코스를 만들고 있어요"
+    ]
+    
+    private let textSubMessages = [
+        "여행에 가져갈 짐 싸는 중...",
+        "당신만을 위한 코스 만드는 중..."
+    ]
+    
+    var body: some View {
+        ZStack {
+            VStack(spacing: 20) {
+                if currentTextIndex == 0 {
+                    VStack(spacing: 8) {
+                        Text(textMessages[currentTextIndex])
+                            .font(.system(size: 24, weight: .semibold))
+                            .foregroundColor(.black)
+                        Text(" ")
+                            .font(.system(size: 24, weight: .semibold))
+                            .foregroundColor(.black)
+                        Text(textSubMessages[currentTextIndex])
+                            .font(.system(size: 18, weight: .semibold))
+                            .foregroundColor(Color(hex: "6E6E6E"))
+                            .padding(.top, 15)
+                    }
+                    .padding()
+                    .padding(.top, 100)
+                } else {
+                    VStack(spacing: 8) {
+                        Text(textMessages[currentTextIndex])
+                            .font(.system(size: 24, weight: .semibold))
+                            .foregroundColor(.black)
+                        Text(textMessages[(currentTextIndex + 1) % textMessages.count])
+                            .font(.system(size: 24, weight: .semibold))
+                            .foregroundColor(.black)
+                        Text(textSubMessages[currentTextIndex])
+                            .font(.system(size: 18, weight: .semibold))
+                            .foregroundColor(Color(hex: "6E6E6E"))
+                            .padding(.top, 15)
+                    }
+                    .padding()
+                    .padding(.top, 100)
+                }
+                
+                Spacer()
+            }
+            
+            LottieView(fileName: "train", loopMode: .loop)
+            .padding(.top, 250)
+        }
+        .padding()
+        .onAppear {
+            startTextRotation()
+        }
+    }
+    
+    private func startTextRotation() {
+        Timer.scheduledTimer(withTimeInterval: 2.5, repeats: true) { _ in
+            withAnimation {
+                currentTextIndex = (currentTextIndex + 1) % textSubMessages.count
+            }
+        }
+    }
+    
+}
