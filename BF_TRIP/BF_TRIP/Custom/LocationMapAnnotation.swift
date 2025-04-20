d1524f19b301507e4d5aaa8ab1a175d15d621079
동재 <59719500+jea0716@users.noreply.github.com>
2025-04-20 16:16:28 +0900

    style : 지도에 나오는 핀 이미지 수정

--- BF_TRIP/BF_TRIP/Custom/LocationMapAnnotation.swift
+++ BF_TRIP/BF_TRIP/Custom/LocationMapAnnotation.swift
@@ -10,23 +10,10 @@ import SwiftUI
 struct LocationMapAnnotation: View {
     var body: some View {
         VStack(spacing: 0) {
-            Image(systemName: "map.circle.fill")
+            Image(uiImage: .mapSign)
                 .resizable()
                 .scaledToFit()
-                .frame(width: 20, height: 20)
-                .font(.headline)
-                .padding(6)
-                .foregroundColor(.white)
-                .background(Color(hex: "#FFE023"))
-                .cornerRadius(36)
-            
-            Image(systemName: "triangle.fill")
-                .resizable()
-                .scaledToFit()
-                .foregroundColor(Color(hex: "#FFE023"))
-                .frame(width: 10, height: 10)
-                .rotationEffect(Angle(degrees: 180))
-                .offset(y: -3)
+                .frame(width: 30, height: 30)
         }
     }
 }
